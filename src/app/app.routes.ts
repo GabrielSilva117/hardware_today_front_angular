eab1d2805d08f0777341a9d6a619e08d652d508b
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-02-26 19:52:21 -0300

    feat: Adds the gallery route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {ProductPageComponent} from './components/product/product-page/product-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'gallery', component: ProductPageComponent },
+];
