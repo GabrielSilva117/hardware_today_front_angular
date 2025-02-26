16e5b05a93d71fbe034fc353f712f730c4b13c5b
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-02-26 19:52:48 -0300

    feat: Adds the product page component to main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {ProductPageComponent} from './app/components/product/product-page/product-page.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
