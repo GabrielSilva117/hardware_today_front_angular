7ac33554241a3cfea28ac34a20229aca9c6bb1ad
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-02-26 19:45:36 -0300

    feat: Adds the product service class

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import FilterModel from '../models/product/FilterModel';
+import axios, {AxiosResponse} from 'axios';
+import {ProductModel} from '../models/product/ProductModel';
+import {environment} from '../../enviroments/enviroment';
+import {BehaviorSubject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private productSource = new BehaviorSubject<ProductModel[]>([]);
+  products$ = this.productSource.asObservable().pipe()
+  api
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: environment.apiUrl + '/products',
+    })
+  }
+
+  getProductList (filter?: FilterModel): Promise<AxiosResponse<ProductModel[]>> {
+    return this.api.post<ProductModel[]>('', filter)
+  }
+
+  setProducts(products: ProductModel[]) {
+    this.productSource.next(products);
+  }
+
+  getProducts() {
+    return this.productSource.getValue();
+  }
+}
