846f3f20ee40409ad98a38242df58f378fe252fc
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-05-22 20:59:16 -0300

    feat: Implements the ControlAccessor in the product combobox component

--- src/app/components/product/product-filter/product-combobox/product-combobox.component.ts
+++ src/app/components/product/product-filter/product-combobox/product-combobox.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule} from '@angular/forms';
 import {MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatOption, MatSelect} from '@angular/material/select';
 
@@ -13,10 +13,46 @@ import {MatOption, MatSelect} from '@angular/material/select';
     MatOption
   ],
   templateUrl: './product-combobox.component.html',
-  styleUrl: './product-combobox.component.css'
+  styleUrl: './product-combobox.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ProductComboboxComponent),
+      multi: true
+    }
+  ]
 })
-export class ProductComboboxComponent {
-  formControl = new FormControl('');
+export class ProductComboboxComponent implements ControlValueAccessor {
+  // formControl = new FormControl('');
   @Input() itemsToList!: Array<any>
   @Input() label!: String;
+  @Input() multiple: boolean = false;
+  @Input() labelKey: string = 'name'
+  @Input() valueKey: string = 'id';
+
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  value: any;
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  setValue(event: any) {
+    this.value = event.value;
+    console.log(this.value);
+    this.onChange(this.value);
+    this.onTouched();
+  }
+
+  protected readonly eval = eval;
 }
