4cf74b649aa876c1501f05ddf1c96910ef7b69da
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-05-22 20:58:18 -0300

    feat: Adds the product price range component

---
+++ src/app/components/product/product-filter/product-price-range/product-price-range.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+
+@Component({
+  selector: 'app-product-price-range',
+  imports: [MatSliderModule],
+  templateUrl: './product-price-range.component.html',
+  styleUrl: './product-price-range.component.css'
+})
+export class ProductPriceRangeComponent {
+
+}
