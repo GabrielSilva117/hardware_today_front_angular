9dd4310d8768487c9b304800d1a4c09ed00d6eba
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-05-22 21:00:17 -0300

    refactor: Changes the product listing

--- src/app/components/product/product-list/product-list.component.ts
+++ src/app/components/product/product-list/product-list.component.ts
@@ -1,36 +1,30 @@
 import {Component, Input} from '@angular/core';
 import {ProductModel} from '../../../models/product/ProductModel';
 import {NgForOf} from '@angular/common';
+import {Router} from '@angular/router';
+import {MatMiniFabButton} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-list',
   imports: [
-    NgForOf
+    NgForOf,
+    MatMiniFabButton,
+    MatIconModule
   ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
   @Input() filteredProducts: ProductModel[] = [];
-  // brands: BrandModel[] = [];
-  // categories: CategoryModel[] = [];
-  //
-  // constructor(private productService: ProductService) { }
-  //
-  // ngOnInit() {
-  //   this.productService.getProductList().then(products => {
-  //     for (let product of products.data) {
-  //       if (this.brands.includes(product.brand)) {
-  //         this.brands.push(product.brand);
-  //       } else if (this.categories.includes(product.category)) {
-  //         this.categories.push(product.category);
-  //       }
-  //     }
-  //
-  //     this.productService.setProducts(products.data)
-  //   }).catch(error => {
-  //     console.error(error);
-  //   })
-  //   this.productService.products$.subscribe(products => {this.filteredProducts = products});
-  // }
+
+  constructor(private router: Router) {}
+
+  openProductPage(productId: string) {
+    this.router.navigate([`/product/${productId}`]);
+  }
+
+  addProductToCart(event: Event) {
+    event.stopPropagation();
+  }
 }
