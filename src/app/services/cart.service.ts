c61fbcd6210d6098e44c6298f88ab3e86ca5141a
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2025-05-29 22:05:36 -0300

    feat: Adds the cart service

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import axios, {AxiosInstance} from 'axios';
+import {environment} from '../../enviroments/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  private api : AxiosInstance;
+
+  constructor() {
+    this.api = axios.create({
+      baseURL: environment.apiUrl + '/cart',
+      withCredentials: true
+    })
+  }
+
+  getAllCartsFromUser() {
+    return this.api.get('/user');
+  }
+
+}
