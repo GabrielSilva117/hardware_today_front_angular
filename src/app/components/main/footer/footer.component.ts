b65b5e8ef9b21210ebed092fdb301fc9274cd94c
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-02-05 20:27:25 -0300

    feat: Changes the footer element

--- src/app/components/main/footer/footer.component.ts
+++ src/app/components/main/footer/footer.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  imports: [CommonModule, MatToolbarModule],
+  template: `
+    <mat-toolbar class="footer">
+      <span>&copy; {{ currentYear }} Hardware Today. All rights reserved.</span>
+    </mat-toolbar>
+  `,
+  styles: [`
+    .footer {
+      background-color: #1e1e1e;
+      color: #ffffff;
+      justify-content: center;
+      margin-top: auto;
+      height: 64px;
+    }
+  `]
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
