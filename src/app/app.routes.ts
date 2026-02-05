2197b9351ba92ecc913bddf4cac51be80d84b861
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-02-05 20:32:53 -0300

    Adds a route to access the payment page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {LoginPageComponent} from './components/login/login-page/login-page.compo
 import {AuthGuard} from './auth.guard';
 import {ProductDetailComponent} from './components/product/product-detail/product-detail.component';
 import {CartPageComponent} from './components/cart/cart-page/cart-page.component';
+import {PaymentPageComponent} from './components/payment/payment-page/payment-page.component';
 
 export const routes: Routes = [
   { path: 'gallery', component: ProductPageComponent, canActivate: [AuthGuard] },
   { path: 'product/:id', component: ProductDetailComponent, canActivate: [AuthGuard] },
   { path: 'cart', component: CartPageComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginPageComponent },
+  { path: 'payment', component: PaymentPageComponent },
   { path: '**', redirectTo: 'login'},
 ];
