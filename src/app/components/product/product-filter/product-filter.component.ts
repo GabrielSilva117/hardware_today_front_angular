3b8adcb33047e877b6550c7b11a749a1ebd4dbdf
Gabriel Ferreira <gabriel.f.silva117@gmail.com>
2026-05-25 20:17:47 -0300

    fix: Fixed filter code

--- src/app/components/product/product-filter/product-filter.component.ts
+++ src/app/components/product/product-filter/product-filter.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {
   MatAccordion,
   MatExpansionPanel, MatExpansionPanelDescription,
@@ -7,16 +7,14 @@ import {
 } from '@angular/material/expansion';
 import {MatButton} from '@angular/material/button';
 import {ProductComboboxComponent} from './product-combobox/product-combobox.component';
-import {ProductModel} from '../../../models/product/ProductModel';
 import {BrandModel} from '../../../models/utils/BrandModel';
 import {CategoryModel} from '../../../models/utils/CategoryModel';
-import {Observable} from 'rxjs';
+import {map, Observable, take, tap} from 'rxjs';
 import {BrandService} from '../../../services/brand.service';
 import {CategoryService} from '../../../services/category.service';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import FilterModel from '../../../models/product/FilterModel';
-import {ProductPriceRangeComponent} from './product-price-range/product-price-range.component';
 import {DefaultFieldComponent} from '../../utils/default-field/default-field.component';
 
 @Component({
@@ -36,33 +34,43 @@ import {DefaultFieldComponent} from '../../utils/default-field/default-field.com
   templateUrl: './product-filter.component.html',
   styleUrl: './product-filter.component.css'
 })
-export class ProductFilterComponent implements OnInit {
+export class ProductFilterComponent {
   @Input() paginatedProdPayload!: Observable<any[]>;
-  myForm!: FormGroup;
+  myForm: FormGroup;
   brands: BrandModel[] = [];
   categories: CategoryModel[] = [];
+  maxPrice: Number = 0;
   @Output() selectProducts = new EventEmitter();
 
-  constructor(private brandService: BrandService, private categoryService: CategoryService, private fb : FormBuilder) {
-    this.brandService.getAllBrands().then(response=> {this.brands = response.data;});
-    this.categoryService.getAllCategories().then(response=> {this.categories = response.data;});
+  constructor(private brandService: BrandService, private categoryService: CategoryService, private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      brand: [[]],
+      category: [[]],
+      maxPrice: [''],
+      minPrice: [''],
+    });
+    this.brandService.getAllBrands().then((response) => {
+      this.brands = response.data;
+    });
+    this.categoryService.getAllCategories().then((response) => {
+      this.categories = response.data;
+    });
   }
 
   sendData() {
     const filterPayload = this.myForm.value;
-    const filterObj: FilterModel = {
-      brand: filterPayload.brand.join(), category: filterPayload.category.join(), maxPrice: filterPayload.maxPrice, minPrice: filterPayload.minPrice
-    }
-
-    return this.selectProducts.emit(filterObj);
-  }
-
-  ngOnInit(): void {
-    this.myForm = this.fb.group({
-      brand: [[]],
-      category: [[]],
-      maxPrice: [[]],
-      minPrice: [[]],
-    })
+    this.paginatedProdPayload.pipe(
+      take(1),
+      map(products => Math.max(...products.map(obj => obj.price)))
+    ).subscribe(maxPrice => {
+      const filterObj: FilterModel = {
+        brand: filterPayload.brand.join(),
+        category: filterPayload.category.join(),
+        maxPrice: Number(maxPrice),
+        minPrice: Number(filterPayload.minPrice) || 0,
+      };
+  
+      this.selectProducts.emit(filterObj);
+    });
   }
 }
